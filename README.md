2fb19436a67531fcec8d2b91a6b9e9d69d403a24
Balram Chavan <balram.chavan@boehringer-ingelheim.com>
2019-07-25 23:26:28 +0200

    added tsconfig and deno types

---
+++ README.md
@@ -0,0 +1,6 @@
+# Deno Server
+
+## Setup for VS Code
+* Create `lib\lib.deno_runtime.d.ts` file.
+* Run `deno types > lib/lib.deno_runtime.d.ts` command to generate deno types
+
--- main.ts
+++ main.ts
@@ -5,8 +5,9 @@ const userService = new UserService();
 const allUsers = userService.getMockUsers();
 // const body = new TextEncoder().encode("Jai Ganesh\n");
 const body = new TextEncoder().encode(JSON.stringify(allUsers));
-const s = serve(":8000");
 
+const s = serve(":8000");
+serve
 window.onload = async () => {
   console.log("http://localhost:8000/");
   for await (const req of s) {
